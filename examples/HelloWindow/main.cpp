7cc792b947fe4dedea95857e8d0fa209dd5d3ff9
Ben <mynameizkevin@gmail.com>
2024-02-27 21:18:59 -0500

    HelloWindow inital start

--- examples/HelloWindow/main.cpp
+++ examples/HelloWindow/main.cpp
@@ -1,12 +1,99 @@
-#include <fmt/core.h>
-#include <fmt/format.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 #include <fmt/os.h>
+
+// Use glm w/ DirectX Settings
+#define GLM_FORCE_SSE42 1
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES 1
+#define GLM_FORCE_LEFT_HANDED
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+bool _running = true;
+
+glm::ivec2 _windowSize = { 800, 600 };
+
+HWND _window = {};
+
+static LRESULT CALLBACK WndProc(HWND hwnd, unsigned msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+        case WM_CLOSE:
+            _running = false;
+            break;
+    }
+
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
 
 int main(int argc, char** argv)
 {
-    fmt::println("{0} {1} {0}", "Hello", "World");
+    DWORD dwStyle = CS_VREDRAW | CS_HREDRAW;
 
-    fmt::println("{:3.2f}", 123.4567);
+    HINSTANCE instance = GetModuleHandle(nullptr);
+
+    const char* windowClassName = "HelloWindowClass";
+
+    WNDCLASSEX wndClass = {
+        .cbSize = sizeof(WNDCLASSEX),
+        .style = dwStyle,
+        .lpfnWndProc = &WndProc,
+        .cbClsExtra = 0,
+        .cbWndExtra = 0,
+        .hInstance = instance,
+        .hIcon = LoadIcon(nullptr, IDI_APPLICATION),
+        .hCursor = LoadCursor(nullptr, IDC_ARROW),
+        .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+        .lpszMenuName = nullptr,
+        .lpszClassName = windowClassName,
+        .hIconSm = nullptr,
+    };
+
+    if (!RegisterClassEx(&wndClass))
+    {
+        throw fmt::windows_error(GetLastError(), "RegisterClassEx() Failed");
+    }
+
+    RECT windowRect = {0, 0, _windowSize.x, _windowSize.y};
+
+    AdjustWindowRect(&windowRect, dwStyle, false);
+
+    const char* title = "HelloWindow";
+
+    _window = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        windowClassName,
+        title,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        windowRect.right - windowRect.left,
+        windowRect.bottom - windowRect.top,
+        nullptr,
+        nullptr,
+        instance,
+        nullptr
+    );
+
+    if (!_window)
+    {
+        throw fmt::windows_error(GetLastError(), "Window was not created successfully");
+    }
+
+    ShowWindow(_window, SW_SHOWNORMAL);
+
+    UpdateWindow(_window);
+
+    while (_running)
+    {
+        MSG msg;
+        if (GetMessage(&msg, NULL, 0, 0)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
     return 0;
 }
